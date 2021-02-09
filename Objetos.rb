01a427d9d8aa7726c3885835151e621a8bd3171d
Denisse Lemos <denu.a.lemos@gmail.com>
2021-02-08 22:46:42 -0300

    agregado objetos

--- Objetos.rb
+++ Objetos.rb
@@ -55,3 +55,33 @@ p = Person2.new("David")
 p.name = "Bob"
 puts p.name
 
+# Resumir el get and set
+class Person3
+  attr_accessor :name # Resumen get y set, si solo quiero un getter uso attr_reader y para un setter attr_writer
+  def initialize(name)
+      @name = name
+  end
+end
+
+p = Person3.new("David")
+p.name = "Bob" # set
+puts p.name # get 
+
+class Person5
+  attr_accessor :name, :age # Resumen get set
+  def initialize(name, age) # Constructor
+      @name = name
+      @age = age
+  end
+  def change(n, a) # Puedo usar esta funcion para cambiar las 2 propiedades de Person5
+      self.name = n
+      self.age = a
+  end
+  def show_info
+      puts "#{self.name} is #{self.age}" # toString, self funciona como this
+  end
+end
+
+p = Person5.new("David", 28)
+p.change("Bob", 42)
+p.show_info
